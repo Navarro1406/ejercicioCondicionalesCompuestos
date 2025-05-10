9c1b18080be117ce2584b1b23e4644dcca68d028
Navarro1406 <andresnavarobenitez2@gmail.com>
2025-05-10 11:04:59 -0500

    Se realiza el ejercicio 7 del taller de condicionales compuestos y queda funcional

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in)
-
+        Scanner sc = new Scanner(System.in);
+        double valorCompra, descuento;
+        System.out.println("Por favor ingrese el valor de su compra en $: ");
+        valorCompra = sc.nextDouble();
+        if (valorCompra > 200000) {
+            descuento = valorCompra * 0.15;
+            System.out.println("El valor de su descuento es:  " + descuento + "$" );
+        } else {
+            System.out.println("Su compra no aplica para descuento.");
+        }
     }
 }
